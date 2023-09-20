a8cb0a77c6a2daaa6d2d5fd2ed3a63ddc8d6bfc6
Thayssa Brock <thayssarbrock@gmail.com>
2023-09-19 22:02:26 -0300

    19-09

--- src/components/cards/CardUser.js
+++ src/components/cards/CardUser.js
@@ -1,40 +1,58 @@
 'use client'
 
-import'./CardUser.css'
-
-const handleDelete = async (idUser) =>{
-
-  const objUser = {
-    id: idUser
-  }
-// url, metodo, o tipo de dado, e os dados
-  const response = await fetch(
-  'http://localhost:3333/user', {
-  cache: 'no-store',
-  method:"DELETE",
-  headers: {
-    "Content-Type": "application/json"
-  },
-  body: JSON.stringify(objUser)
-})
-  return console.log(await response.json())
-  //return await response.json()
-}
+import {useState} from 'react'
+import './CardUser.css'
 
 const CardUser = ({user}) => {
+
+  const [showCard, setShowCard] = useState(true)
+
+  const handleDelete = async (idUser) => {
+
+    const objUser = {
+      id: idUser
+    }
+  
+    const response = await fetch('http://localhost:3333/user',
+      {
+        cache: 'no-store',
+        method: 'DELETE',
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(objUser)
+      }
+    )
+    
+    if(response.ok){
+      const result = await response.json()
+      if(result?.success){
+        setShowCard(false)
+      }
+    }
+  }
+
+  
   return (
+    <>
+      {
+        showCard ?
         <div className="card-user">
-        <img src={user.photo} width="70px" height="70px" alt={user.name}></img>
-    <div>
-        <h4>{user.name}</h4>
-        <p>{user.email}</p>
-        <p>
-          <span className='delete' onClick={() => handleDelete(user.id)}>Excluir</span>
-          <span>Editar</span>
-        </p>
-    </div>
-    </div>
+        <img src={user.photo} alt={user.name} width="70px" height="70px" />
+          <div>
+              <h4>{user.name}</h4>
+              <p>{user.email}</p>
+              <p>
+                <span className="delete" onClick={() => handleDelete(user.id)}>Excluir</span>
+                <span>Editar</span>
+              </p>
+          </div>
+        </div>
+      : null
+      }
+    </>
   )
 }
+
 
 export default CardUser
